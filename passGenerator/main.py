13c57dd6c8dc153cc6c6b042b196ba9737a81888
andrea-artuso <ultrandr3x@gmail.com>
2020-11-22 14:17:40 +0100

    edited main.py

--- passGenerator/main.py
+++ passGenerator/main.py
@@ -2,17 +2,20 @@ import random
 
 input1 = input("Write the length of password: ")
 length = int(input1)
+input2 = input("Do you want symbols? [y/n]")
 password = ''
 n1 = ''
 
 while len(password) < length:
-    cas = random.randint(0, 2)
+    cas = random.randint(0, 3)
     if cas == 0:
         n1 = random.randint(48, 57)
     elif cas == 1:
         n1 = random.randint(65, 90)
     elif cas == 2:
         n1 = random.randint(97, 122)
+    elif cas == 3 and input2 == "y":
+        n1 = random.randint(33, 38)
 
     val = chr(n1)
     password += val
